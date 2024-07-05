837dd3a0826587b29955285c4c3e54218e70a98d
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-07-05 14:38:34 +0800

    更新

---
+++ Library.lua
@@ -0,0 +1,4 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/VIP/main/Fluent.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/VIP/main/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/VIP/main/InterfaceManager.lua"))()
+return Library
