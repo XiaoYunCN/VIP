d1716efa7a2db9f7c16e7e9c892bf25c03afe28f
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-07-31 03:55:26 +0800

    Delete Library.lua

--- Library.lua
+++
@@ -1,4 +0,0 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/VIP/main/Fluent.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/VIP/main/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/VIP/main/InterfaceManager.lua"))()
-return Library
