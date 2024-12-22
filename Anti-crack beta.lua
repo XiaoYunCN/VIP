92b159545bb870eedc2a8ff2b39a379ce58c92aa
lua <130466576+XiaoYunCN@users.noreply.github.com>
2024-12-22 20:46:08 +0800

    Delete Anti-crack beta.lua

--- Anti-crack beta.lua
+++
@@ -1,3 +0,0 @@
---Whitelist User Name：Hudson_From4
---Make by XA Hub
-loadstring(game:HttpGet("https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/%E9%98%B2%E7%A0%B4%E8%A7%A3-%E6%B5%8B%E8%AF%95.lua", true))()
