80469aa671307176f88b4125218034129ec19975
lua <130466576+XiaoYunCN@users.noreply.github.com>
2024-12-22 18:07:28 +0800

    更新 Anti-crack beta.lua

--- Anti-crack beta.lua
+++ Anti-crack beta.lua
@@ -1,2 +1,3 @@
---白名单用户名：Hudson_From4
+--Whitelist User Name：Hudson_From4
+--Make by XA
 loadstring(game:HttpGet("https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/%E9%98%B2%E7%A0%B4%E8%A7%A3-%E6%B5%8B%E8%AF%95.lua", true))()
