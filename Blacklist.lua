882b914f1e00a5abbcbd010b52d61718fa00206b
XiaoYunDev <130466576+XiaoYunCN@users.noreply.github.com>
2025-02-01 14:49:20 +0800

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,15 +1,26 @@
 return {
-    ["f0d2de553bddaadf630f3fe0409a5ee9798de325fa542c0fe8464aa4d1f11662"] = "nm",
-    ["4fe7083f657ec681a6c2554acd0365a9e3a1885116e2ecd174270105449fb3b0"] = "😡",
-    ["64"] = "1111",
-    ["2"] = "11",
-    ["0c4224301005c0e3"] = "其他",
-    ["66a9f4ba7c1faa17"] = "？",
-    ["Hwid_Example4"] = "其他",
-    ["Hwid_Example5"] = "其他",
-    ["Hwid_Example6"] = "其他",
-    ["Hwid_Example7"] = "其他",
-    ["Hwid_Example8"] = "其他",
-    ["Hwid_Example9"] = "其他",
-    ["Hwid_Example10"] = "其他"
+    NAMEID = {
+        ["123456789"] = "ks",
+        ["113"] = "s"
+    },
+    JOINDATE = {
+        ["2023/6/1"] = "6",
+        ["2024/12/31"] = "k"
+    },
+    USERNAME = {
+        ["Player1"] = "nm",
+        ["Roblox"] = "wc"
+    },
+    CLIENTID = {
+        ["ABCD-1234-EFGH"] = "6",
+        ["XYZ-9876-TUVW"] = "k"
+    },
+    HWID = {
+        ["f0d2de553bddaadf630f3fe0409a5ee9798de325fa542c0fe8464aa4d1f11662"] = "😡",
+        ["4fe7083f657ec681a6c2554acd0365a9e3a1885116e2ecd174270105449fb3b0"] = "😡",
+        ["64"] = "1111",
+        ["2"] = "11",
+        ["0c4224301005c0e3"] = "...",
+        ["66a9f4ba7c1faa17"] = "？"
+    }
 }
